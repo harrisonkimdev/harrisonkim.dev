b04520968aba1769af037c0d178e5ff6d9db3a4a
Harrison Kim <hsookim90@gmail.com>
2023-05-15 20:30:41 -0400

    Rearrange the layout

---
+++ app/components/Footer.tsx
@@ -0,0 +1,71 @@
+'use client'
+
+import React from 'react'
+import { usePathname } from 'next/navigation'
+import Link from 'next/link'
+import Image from 'next/image'
+
+const Footer = () => {
+  const currentUrl = () => {
+    return usePathname() == '/' ? false : true
+  }
+
+  return (
+    <>
+      { currentUrl() && (
+        <div className='flex flex-row gap-56 py-10 px-20 bg-zinc-200'>
+          <div className='flex flex-col gap-4'>
+            <img
+              src='https://dieselpunkcore.com/wp-content/uploads/2014/06/logo-placeholder.png'
+              alt='' className='object-contain w-12'
+            />
+            <div className='flex flex-col gap-2'>
+              <Link href='https://www.linkedin.com/in/harrison-kim-b246a5175/'
+                className='flex flex-row gap-2 items-center hover:underline'
+              >
+                <Image src='/images/logos/linkedin-logo.png'
+                  alt='LinkedIn Logo' width={16} height={16} className='object-contain'
+                />
+                <span>LinkedIn</span>
+              </Link>
+              <Link href='https://www.linkedin.com/in/harrison-kim-b246a5175/'
+                className='flex flex-row gap-2 items-center hover:underline'
+              >
+                <Image src='/images/logos/github-logo.png'
+                  alt='GitHub Logo' width={16} height={16} className='object-contain'
+                />
+                <span>GitHub</span>
+              </Link>
+            </div>
+          </div>
+
+          <div className='flex flex-row gap-32'>
+            <div className='flex flex-col gap-4'>
+              <p className='text-3xl font-bold text-zinc-700'>Home</p>
+              <ul className='flex flex-col gap-2'>
+                <li className='hover:underline'><Link href='/resume'>Resume</Link></li>
+                <li className='hover:underline'><Link href='/projects'>Projects</Link></li>
+              </ul>
+            </div>
+            <div className='flex flex-col gap-4'>
+              <p className='text-3xl font-bold text-zinc-700'>Projects</p>
+              <ul className='flex flex-col gap-2'>
+                <li className='hover:underline'><Link href='#'>mosPic</Link></li>
+                <li className='hover:underline'><Link href='#'>Dashboard</Link></li>
+              </ul>
+            </div>
+            <div className='flex flex-col gap-4'>
+              <p className='text-3xl font-bold text-zinc-700'>About</p>
+              <ul className='flex flex-col gap-2'>
+                <li className='hover:underline'><Link href='/about?tab=myself'>Myself</Link></li>
+                <li className='hover:underline'><Link href='/about?tab=website'>This website</Link></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  )
+}
+
+export default Footer
--- app/page.tsx
+++ app/page.tsx
@@ -2,34 +2,33 @@ import './styles/welcome.css'
 
 export default function Home() {
   return (
-    <div className='wrapper'>
-      <img src="/images/space.jpg" alt="" className='background' />
-      <header className='relaitve'>
-        <img src="/images/astronaut.png" alt="" className='foreground' />
-        <div className='text-center'>
-          <p className='title text-5xl font-extrabold'>Harrison Kim</p>
-          <p className='title text-xl font-bold'>Full Stack Engineer</p>
-        </div>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 absolute bottom-8 text-white">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />
-        </svg>
-      </header>
+    <>
+      {/* <div className={toggleSidebar ? 'opacity-20' : ''}> */}
+      <div className='wrapper'>
+        <img src="/images/space.jpg" alt="" className='background' />
+        <header className='relaitve'>
+          <img src="/images/astronaut.png" alt="" className='foreground' />
+          <div className='text-center'>
+            <p className='title text-5xl font-extrabold'>Harrison Kim</p>
+            <p className='title text-xl font-bold'>Full Stack Engineer</p>
+          </div>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 absolute bottom-8 text-white">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />
+          </svg>
+        </header>
 
-      <section className='flex flex-col gap-6'>
-        <div className='max-w-sm md:max-w-lg lg:max-w-3xl mx-auto'>
-          <p className='text-2xl text-center'>Hello, world!</p>
-          <p className='mt-3'>My name is <a href="https://www.linkedin.com/in/harrison-kim-b246a5175/" className='hover:underline'>Harrison Kim</a> and
-            I am a <a href="https://github.com/hhkimmm9" className='hover:underline'>full-stack engineer</a>.
-          </p>
-          
-        </div>
-        <div className='flex justify-center my-2'>
-          <p><a href="/home" className='hover:text-2xl ease-in duration-100'>Welcome to my website!</a></p>
-          {/* <button className='px-2 py-1 bg-zinc-300 shadow rounded-xl text-black hover:bg-zinc-200'>
-            Enter
-          </button> */}
-        </div>
-      </section>
-    </div>
+        <section className='flex flex-col gap-6'>
+          <div className='max-w-sm md:max-w-lg lg:max-w-3xl mx-auto'>
+            <p className='text-2xl text-center'>Hello, world!</p>
+            <p className='mt-3'>My name is <a href="https://www.linkedin.com/in/harrison-kim-b246a5175/" className='hover:underline'>Harrison Kim</a> and
+              I am a <a href="https://github.com/hhkimmm9" className='hover:underline'>full-stack engineer</a>.
+            </p>
+          </div>
+          <div className='flex justify-center my-2'>
+            <p><a href="/home" className='hover:text-2xl ease-in duration-100'>Welcome to my website!</a></p>
+          </div>
+        </section>
+      </div>
+    </>
   )
 }
