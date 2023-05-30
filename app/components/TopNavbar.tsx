c9702116c959f900efaea1d0f73602ee89c99f61
Harrison Kim <hsookim90@gmail.com>
2023-05-30 10:47:46 -0400

    Remove dark mode from the top nav bar

--- app/components/TopNavbar.tsx
+++ app/components/TopNavbar.tsx
@@ -62,21 +62,21 @@ const TopNavbar = ( props: any ) => {
               
               {/* Smartphones */}
               <div className={toggleSidebar ? 'opacity-20' : ''}>
-                <div className='flex md:hidden justify-between p-4 items-center bg-black text-stone-200'>
-                  <div onClick={() => { setToggleSidebar(true); props.emitCloseSidebar(true); }}>
+                <div className='grid md:hidden grid-cols-3 p-4 items-center bg-black text-stone-200'>
+                  <div onClick={() => { setToggleSidebar(true); props.emitCloseSidebar(true); }} className='col-span-1'>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                       <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                     </svg>
                   </div>
     
-                  <Link href="/home"> 
+                  <Link href="/home" className='col-span-1 flex justify-center'> 
                     <Image src='/images/logos/dev-kim-transparent.png' alt='Dev Kim Logo'
                       width={112} height={112} className='object-contain w-28 invert'
                     />
                   </Link>
     
                   {/* dark mode toggle */}
-                  <div onClick={() => {}}>
+                  {/* <div onClick={() => {}}>
                     { !darkMode ?
                       (
                         // light moon
@@ -90,7 +90,7 @@ const TopNavbar = ( props: any ) => {
                         </svg>
                       )
                     }
-                  </div>
+                  </div> */}
                 </div>
               </div>
             </nav>
