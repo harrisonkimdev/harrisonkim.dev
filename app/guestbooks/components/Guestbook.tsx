c8fef44e18fbff29f3075da2d0c5e528c321e705
Harrison Kim <hsookim90@gmail.com>
2023-09-19 20:33:19 -0400

    Replace heroicons with react-icons.

--- app/guestbooks/components/Guestbook.tsx
+++ app/guestbooks/components/Guestbook.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { IGuestbook } from '../interfaces'
 import Link from 'next/link'
 import '@/globals.css'
+import { FaUser } from 'react-icons/fa'
 
 const Guestbook = ( props: { guestbookData: IGuestbook } ) => {
 
@@ -48,11 +49,9 @@ const Guestbook = ( props: { guestbookData: IGuestbook } ) => {
           {/* writer and created at */}
           <div className='flex flex-row justify-between gap-6 items-end pt-1'>
             {/* writer */}
-            <div className='flex flex-row gap-1 items-center'>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
-              </svg>
-              <p className='whitespace-nowrap'>{ props.guestbookData.writer }</p>
+            <div className='flex flex-row gap-2 items-center'>
+              <FaUser />
+              <span className='whitespace-nowrap'>{ props.guestbookData.writer }</span>
             </div>
 
             {/* days ago */}
--- app/guestbooks/page.tsx
+++ app/guestbooks/page.tsx
@@ -40,7 +40,7 @@ const Page = () => {
 
       {/* pagination */}
       <div>
-        
+
       </div>
     </>
   )
