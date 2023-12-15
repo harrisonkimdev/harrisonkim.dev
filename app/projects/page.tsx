5d16b1ff21664bdc568dfab6223b4060e9d618d2
Harrison Kim <29724111+hhkimmm9@users.noreply.github.com>
2023-12-15 10:21:51 -0500

    Toggle the visibility of the terminal window when a tab is clicked.

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -4,6 +4,7 @@ import React, { useState } from 'react'
 import '@/globals.css'
 import ProjectContent from '@/components/ProjectContent'
 import '@/assets/project_description.json'
+import { FaTerminal } from "react-icons/fa6"
 
 const Page = () => {
     const [filepath, setFilepath] = useState('')
@@ -25,73 +26,84 @@ const Page = () => {
             return <ProjectContent project={projects_json.projects[3]} />
     }
 
-    const tabClicked = (projectName: string) => {
+    const tabClicked = (projectName: string, showContent: boolean) => {
         setFilepath(projectName)
-        setIsContentVisible(true)
+        setIsContentVisible(showContent)
     }
 
   return (
     <div className='bg-stone-50 w-full px-4'>
-        <div className='max-w-5xl mx-auto'>
-            <div className=''>
-                {/* a group of tabs for projects */}
-                <div className='flex justify-between'>
-                <button type='button' onClick={() => { tabClicked('dandycv') }}
-                    className={`
-                    ${filepath === 'dandycv' ? 'bg-stone-300' : 'bg-stone-200'}
-                    w-full
-                    py-1
-                    cursor-pointer
-                    rounded-tl-md
-                    hover:bg-stone-300
-                `}> dandycv </button>
+        <div className='max-w-5xl mx-auto py-12'>
 
-                <button type='button' onClick={() => { tabClicked('mosPic') }}
-                    className={`
-                    ${filepath === 'mosPic' ? 'bg-stone-300' : 'bg-stone-200'}
-                    w-full
-                    py-1
-                    cursor-pointer
-                    hover:bg-stone-300
-                `}> mosPic </button>
-
-                {/* rounded-tr-md */}
-                <button type='button' onClick={() => { tabClicked('dashboard') }}
-                    className={`
-                    ${filepath === 'Dashboard' ? 'bg-stone-300' : 'bg-stone-200'}
-                    w-full
-                    py-1
-                    cursor-pointer
-                    hover:bg-stone-300
-                `}> Dashboard </button>
-
-                <button type='button' onClick={() => { tabClicked('claw_machine') }}
-                    className={`
-                    ${filepath === 'claw_machine' ? 'bg-stone-300' : 'bg-stone-200'}
-                    w-full
-                    py-1
-                    cursor-pointer
-                    rounded-tr-md
-                    hover:bg-stone-300
-                `}> Claw Machine </button>
+            {/* terminal window */}
+            { isContentVisible ? (
+                <div className='h-16 py-4 flex justify-center'>
+                    <FaTerminal onClick={() => { tabClicked('', false) }}
+                        className='text-2xl cursor-pointer hover:text-4xl ease-in duration-100'
+                    />
                 </div>
-                {/* terminal window */}
-                <div className='
-                h-72 px-2 pt-1 pb-20 rounded-b-md bg-black
-                font-mono text-lime-400
-                '>
-                <p>
-                    user@harrisonkim-dev ~ % git clone { filepath }
-                    <span className='blink_me'>|</span>
-                </p>
-                {/* <p>Downloading...</p>
-                <p>Installing the program...</p>
-                <p>Initializing...</p>
-                <p>Hang on tight... Almost ready...</p>
-                <p>Happy coding!</p> */}
+            ) : (
+                <div className=''>
+                    {/* a group of tabs for projects */}
+                    <div className='flex justify-between'>
+                    <button type='button' onClick={() => { tabClicked('dandycv', true) }}
+                        className={`
+                        ${filepath === 'dandycv' ? 'bg-stone-300' : 'bg-stone-200'}
+                        w-full
+                        py-1
+                        cursor-pointer
+                        rounded-tl-md
+                        hover:bg-stone-300
+                    `}> dandycv </button>
 
+                    <button type='button' onClick={() => { tabClicked('mosPic', true) }}
+                        className={`
+                        ${filepath === 'mosPic' ? 'bg-stone-300' : 'bg-stone-200'}
+                        w-full
+                        py-1
+                        cursor-pointer
+                        hover:bg-stone-300
+                    `}> mosPic </button>
+
+                    {/* rounded-tr-md */}
+                    <button type='button' onClick={() => { tabClicked('dashboard', true) }}
+                        className={`
+                        ${filepath === 'Dashboard' ? 'bg-stone-300' : 'bg-stone-200'}
+                        w-full
+                        py-1
+                        cursor-pointer
+                        hover:bg-stone-300
+                    `}> Dashboard </button>
+
+                    <button type='button' onClick={() => { tabClicked('claw_machine', true) }}
+                        className={`
+                        ${filepath === 'claw_machine' ? 'bg-stone-300' : 'bg-stone-200'}
+                        w-full
+                        py-1
+                        cursor-pointer
+                        rounded-tr-md
+                        hover:bg-stone-300
+                    `}> Claw Machine </button>
+                    </div>
+                    {/* terminal */}
+                    <div className='
+                    h-72 px-2 pt-1 pb-20 rounded-b-md bg-black
+                    font-mono text-lime-400
+                    '>
+                    <p>
+                        user@harrisonkim-dev ~ % git clone { filepath }
+                        <span className='blink_me'>|</span>
+                    </p>
+                    {/* <p>Downloading...</p>
+                    <p>Installing the program...</p>
+                    <p>Initializing...</p>
+                    <p>Hang on tight... Almost ready...</p>
+                    <p>Happy coding!</p> */}
+
+                    </div>
                 </div>
-            </div>
+            )}
+            
 
             {/* Project Description */}
             { isContentVisible && (
