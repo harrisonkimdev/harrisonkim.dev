1e5490f7f7119975bfe2221422bb08a7badd4a52
Harrison Kim <29724111+hhkimmm9@users.noreply.github.com>
2023-12-15 10:10:55 -0500

    Add a function to handle tab selection within the terminal window.

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -7,6 +7,7 @@ import '@/assets/project_description.json'
 
 const Page = () => {
     const [filepath, setFilepath] = useState('')
+    const [isContentVisible, setIsContentVisible] = useState(false)
 
     const projects_json = require('@/assets/project_description.json')
 
@@ -14,7 +15,7 @@ const Page = () => {
         if (filepath === 'claw_machine')
          return <ProjectContent project={projects_json.projects[0]} />
 
-        else if (filepath === 'Dashboard')
+        else if (filepath === 'dashboard')
             return <ProjectContent project={projects_json.projects[1]} />
 
         else if (filepath === 'mosPic')
@@ -24,71 +25,80 @@ const Page = () => {
             return <ProjectContent project={projects_json.projects[3]} />
     }
 
+    const tabClicked = (projectName: string) => {
+        setFilepath(projectName)
+        setIsContentVisible(true)
+    }
+
   return (
     <div className='bg-stone-50 w-full px-4'>
         <div className='max-w-5xl mx-auto'>
-        <div className=''>
-            {/* a group of tabs for projects */}
-            <div className='flex justify-between'>
-            <button type='button' onClick={() => {setFilepath('dandycv')}}
-                className={`
-                ${filepath === 'dandycv' ? 'bg-stone-300' : 'bg-stone-200'}
-                w-full
-                py-1
-                cursor-pointer
-                rounded-tl-md
-                hover:bg-stone-300
-            `}> dandycv </button>
+            <div className=''>
+                {/* a group of tabs for projects */}
+                <div className='flex justify-between'>
+                <button type='button' onClick={() => { tabClicked('dandycv') }}
+                    className={`
+                    ${filepath === 'dandycv' ? 'bg-stone-300' : 'bg-stone-200'}
+                    w-full
+                    py-1
+                    cursor-pointer
+                    rounded-tl-md
+                    hover:bg-stone-300
+                `}> dandycv </button>
 
-            <button type='button' onClick={() => {setFilepath('mosPic')}}
-                className={`
-                ${filepath === 'mosPic' ? 'bg-stone-300' : 'bg-stone-200'}
-                w-full
-                py-1
-                cursor-pointer
-                hover:bg-stone-300
-            `}> mosPic </button>
+                <button type='button' onClick={() => { tabClicked('mosPic') }}
+                    className={`
+                    ${filepath === 'mosPic' ? 'bg-stone-300' : 'bg-stone-200'}
+                    w-full
+                    py-1
+                    cursor-pointer
+                    hover:bg-stone-300
+                `}> mosPic </button>
 
-            {/* rounded-tr-md */}
-            <button type='button' onClick={() => {setFilepath('Dashboard')}}
-                className={`
-                ${filepath === 'Dashboard' ? 'bg-stone-300' : 'bg-stone-200'}
-                w-full
-                py-1
-                cursor-pointer
-                hover:bg-stone-300
-            `}> Dashboard </button>
+                {/* rounded-tr-md */}
+                <button type='button' onClick={() => { tabClicked('dashboard') }}
+                    className={`
+                    ${filepath === 'Dashboard' ? 'bg-stone-300' : 'bg-stone-200'}
+                    w-full
+                    py-1
+                    cursor-pointer
+                    hover:bg-stone-300
+                `}> Dashboard </button>
 
-            <button type='button' onClick={() => {setFilepath('claw_machine')}}
-                className={`
-                ${filepath === 'claw_machine' ? 'bg-stone-300' : 'bg-stone-200'}
-                w-full
-                py-1
-                cursor-pointer
-                rounded-tr-md
-                hover:bg-stone-300
-            `}> Claw Machine </button>
+                <button type='button' onClick={() => { tabClicked('claw_machine') }}
+                    className={`
+                    ${filepath === 'claw_machine' ? 'bg-stone-300' : 'bg-stone-200'}
+                    w-full
+                    py-1
+                    cursor-pointer
+                    rounded-tr-md
+                    hover:bg-stone-300
+                `}> Claw Machine </button>
+                </div>
+                {/* terminal window */}
+                <div className='
+                h-72 px-2 pt-1 pb-20 rounded-b-md bg-black
+                font-mono text-lime-400
+                '>
+                <p>
+                    user@harrisonkim-dev ~ % git clone { filepath }
+                    <span className='blink_me'>|</span>
+                </p>
+                {/* <p>Downloading...</p>
+                <p>Installing the program...</p>
+                <p>Initializing...</p>
+                <p>Hang on tight... Almost ready...</p>
+                <p>Happy coding!</p> */}
+
+                </div>
             </div>
-            {/* terminal window */}
-            <div className='
-            h-72 px-2 pt-1 pb-20 rounded-b-md bg-black
-            font-mono text-lime-400
-            '>
-            <p>
-                user@harrisonkim-dev ~ % git clone { filepath }
-                <span className='blink_me'>|</span>
-            </p>
-            {/* <p>Downloading...</p>
-            <p>Installing the program...</p>
-            <p>Initializing...</p>
-            <p>Hang on tight... Almost ready...</p>
-            <p>Happy coding!</p> */}
 
-            </div>
-        </div>
-        <div className='py-6'>
-            { contentWrapper() }
-        </div>
+            {/* Project Description */}
+            { isContentVisible && (
+                <div className='py-6'>
+                    { contentWrapper() }
+                </div>
+            )}
         </div>
     </div>
     )
