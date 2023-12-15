ed90012ebf4629c554d3ea132f3f7b86bf9f6392
Harrison Kim <29724111+hhkimmm9@users.noreply.github.com>
2023-12-15 10:04:33 -0500

    Change the indentation within the Project page.

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -6,92 +6,92 @@ import ProjectContent from '@/components/ProjectContent'
 import '@/assets/project_description.json'
 
 const Page = () => {
-  const [filepath, setFilepath] = useState('')
+    const [filepath, setFilepath] = useState('')
 
-  const projects_json = require('@/assets/project_description.json')
+    const projects_json = require('@/assets/project_description.json')
 
-  const contentWrapper = () => {
-    if (filepath === 'claw_machine')
-      return <ProjectContent project={projects_json.projects[0]} />
+    const contentWrapper = () => {
+        if (filepath === 'claw_machine')
+         return <ProjectContent project={projects_json.projects[0]} />
 
-    else if (filepath === 'Dashboard')
-      return <ProjectContent project={projects_json.projects[1]} />
+        else if (filepath === 'Dashboard')
+            return <ProjectContent project={projects_json.projects[1]} />
 
-    else if (filepath === 'mosPic')
-      return <ProjectContent project={projects_json.projects[2]} />
+        else if (filepath === 'mosPic')
+            return <ProjectContent project={projects_json.projects[2]} />
     
-    else if (filepath === 'dandycv')
-      return <ProjectContent project={projects_json.projects[3]} />
-  }
+        else if (filepath === 'dandycv')
+            return <ProjectContent project={projects_json.projects[3]} />
+    }
 
   return (
-    <>
-      <div className='bg-stone-50 w-full px-4'>
+    <div className='bg-stone-50 w-full px-4'>
         <div className='max-w-5xl mx-auto'>
-          <div className='py-10'>
+        <div className=''>
+            {/* a group of tabs for projects */}
             <div className='flex justify-between'>
-              <button type='button' onClick={() => {setFilepath('dandycv')}}
+            <button type='button' onClick={() => {setFilepath('dandycv')}}
                 className={`
-                  ${filepath === 'dandycv' ? 'bg-stone-300' : 'bg-stone-200'}
-                  w-full
-                  py-1
-                  cursor-pointer
-                  rounded-tl-md
+                ${filepath === 'dandycv' ? 'bg-stone-300' : 'bg-stone-200'}
+                w-full
+                py-1
+                cursor-pointer
+                rounded-tl-md
                 hover:bg-stone-300
-              `}> dandycv </button>
+            `}> dandycv </button>
 
-              <button type='button' onClick={() => {setFilepath('mosPic')}}
+            <button type='button' onClick={() => {setFilepath('mosPic')}}
                 className={`
-                  ${filepath === 'mosPic' ? 'bg-stone-300' : 'bg-stone-200'}
-                  w-full
-                  py-1
-                  cursor-pointer
+                ${filepath === 'mosPic' ? 'bg-stone-300' : 'bg-stone-200'}
+                w-full
+                py-1
+                cursor-pointer
                 hover:bg-stone-300
-              `}> mosPic </button>
+            `}> mosPic </button>
 
-              {/* rounded-tr-md */}
-              <button type='button' onClick={() => {setFilepath('Dashboard')}}
+            {/* rounded-tr-md */}
+            <button type='button' onClick={() => {setFilepath('Dashboard')}}
                 className={`
-                  ${filepath === 'Dashboard' ? 'bg-stone-300' : 'bg-stone-200'}
-                  w-full
-                  py-1
-                  cursor-pointer
+                ${filepath === 'Dashboard' ? 'bg-stone-300' : 'bg-stone-200'}
+                w-full
+                py-1
+                cursor-pointer
                 hover:bg-stone-300
-              `}> Dashboard </button>
+            `}> Dashboard </button>
 
-              <button type='button' onClick={() => {setFilepath('claw_machine')}}
+            <button type='button' onClick={() => {setFilepath('claw_machine')}}
                 className={`
-                  ${filepath === 'claw_machine' ? 'bg-stone-300' : 'bg-stone-200'}
-                  w-full
-                  py-1
-                  cursor-pointer
-                  rounded-tr-md
+                ${filepath === 'claw_machine' ? 'bg-stone-300' : 'bg-stone-200'}
+                w-full
+                py-1
+                cursor-pointer
+                rounded-tr-md
                 hover:bg-stone-300
-              `}> Claw Machine </button>
+            `}> Claw Machine </button>
             </div>
+            {/* terminal window */}
             <div className='
-              h-72 px-2 pt-1 pb-20 rounded-b-md bg-black
-              font-mono text-lime-400
+            h-72 px-2 pt-1 pb-20 rounded-b-md bg-black
+            font-mono text-lime-400
             '>
-              <p>
+            <p>
                 user@harrisonkim-dev ~ % git clone { filepath }
                 <span className='blink_me'>|</span>
-              </p>
-              {/* <p>Downloading...</p>
-              <p>Installing the program...</p>
-              <p>Initializing...</p>
-              <p>Hang on tight... Almost ready...</p>
-              <p>Happy coding!</p> */}
+            </p>
+            {/* <p>Downloading...</p>
+            <p>Installing the program...</p>
+            <p>Initializing...</p>
+            <p>Hang on tight... Almost ready...</p>
+            <p>Happy coding!</p> */}
 
             </div>
-          </div>
-          <div className='py-6'>
-            { contentWrapper() }
-          </div>
         </div>
-      </div>
-    </>
-  )
+        <div className='py-6'>
+            { contentWrapper() }
+        </div>
+        </div>
+    </div>
+    )
 }
 
 export default Page
