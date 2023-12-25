17944068e5e9635b18b4ab7117a145ff8a701f9b
Harrison Kim <29724111+hhkimmm9@users.noreply.github.com>
2023-12-24 20:19:21 -0500

    Add background objects.

--- app/components/DinosaurGame.tsx
+++ app/components/DinosaurGame.tsx
@@ -12,23 +12,33 @@ const DinosaurGame = () => {
         const canvas: HTMLElement | null = document.getElementById('canvas')
         var ctx: CanvasRenderingContext2D | null
 
+        // bug & person image
         var bugImage = new Image()
         bugImage.src = '/images/bug.png'
+        var personImage = new Image()
+        personImage.src = '/images/running-person.png'
+        var zeroImage = new Image()
+        zeroImage.src = '/images/zero.png'
+        var oneImage = new Image()
+        oneImage.src = '/images/one.png'
 
         var timer = 0
         var bugs: Bug[] = []
+        var numbers: Number[] = []
         var isJumping = false
-        var periodJumping = 0
+        var score = 0
 
-        var dino = {
+        // 
+        var player = {
             x: 50,
             y: 75,
-            width: 50,
+            width: 65,
             height: 75,
             draw() {
                 if (ctx) {
                     ctx.fillStyle = 'green'
                     ctx?.fillRect(this.x, this.y, this.width, this.height)
+                    ctx.drawImage(personImage, this.x, this.y, this.width, this.height)
                 }
             },
             jump() {
@@ -45,6 +55,40 @@ const DinosaurGame = () => {
             }
         }
 
+        // BackgroundImages
+        // TODO: consider implementing it with Interface
+        class Number {
+            private x: number
+            private y: number
+            private width: number
+            private height: number
+
+            constructor() {
+                this.x = window.innerWidth
+                this.y = Math.floor(Math.random() * 75)
+                this.width = 25
+                this.height = 25
+            }
+            draw() {
+                var zeroOrOne = Math.floor(Math.random() * 2)
+
+                if (ctx) {
+                    ctx.fillStyle = 'yellow'
+                    ctx?.fillRect(this.x, this.y, this.width, this.height)
+                    if (zeroOrOne === 0)
+                        ctx?.drawImage(zeroImage, this.x, this.y, this.width, this.height)
+                    else
+                        ctx?.drawImage(oneImage, this.x, this.y, this.width, this.height)
+                }
+            }
+            moveForward() {
+                this.x -= (5 + Math.floor(Math.random()*4))
+            }
+            isOutOfFrame() {
+                return this.x < 0 ? true : false
+            }
+        }
+
         class Bug {
             private x: number
             private y: number
@@ -52,21 +96,20 @@ const DinosaurGame = () => {
             private height: number
 
             constructor() {
-                // this.x = window.innerWidth
-                this.x = 500
+                this.x = window.innerWidth
                 this.y = 125
                 this.width = 25
                 this.height = 25
             }
             draw() {
                 if (ctx) {
-                    // ctx.fillStyle = 'red'
-                    // ctx?.fillRect(this.x, this.y, this.width, this.height)
-                    ctx?.drawImage(bugImage, this.x, this.y)
+                    ctx.fillStyle = 'red'
+                    ctx?.fillRect(this.x, this.y, this.width, this.height)
+                    ctx?.drawImage(bugImage, this.x, this.y, this.width, this.height)
                 }
             }
             moveForward() {
-                this.x--
+                this.x -= 3 + Math.floor(Math.random()*1)
             }
             isOutOfFrame() {
                 return this.x < 0 ? true : false
@@ -82,27 +125,44 @@ const DinosaurGame = () => {
                 ctx?.clearRect(0,0, canvas.width, canvas.height)
             }
 
-            if (timer % (75*2) === 0) {
+            // object generators
+            // bugs
+            if (timer % (75*(2 + Math.floor(Math.random()*3))) === 0) {
                 var bug = new Bug()
                 bugs.push(bug)
             }
+            // numbers
+            if (timer % (25 + Math.floor(Math.random()*5)) === 0) {
+                var number = new Number()
+                numbers.push(number)
+            }
 
+            // object dispatcher
+            // bugs
             bugs.forEach((bug, i, o) => {
                 if (bug.isOutOfFrame())
                     o.splice(i, 1)
                 bug.moveForward()
                 bug.draw()
             })
+            // numbers
+            numbers.forEach((number, i, o) => {
+                if (number.isOutOfFrame())
+                    o.splice(i, 1)
+                number.moveForward()
+                number.draw()
+            })
 
-            dino.draw()
+            player.draw()
 
+            // jump action
             if (isJumping === true) {
-                if (dino.didHitTheCeiling())
+                if (player.didHitTheCeiling())
                     isJumping = false
-                dino.jump()
+                player.jump()
             } else {
-                if (!dino.isOnTheGround())
-                    dino.fall()
+                if (!player.isOnTheGround())
+                    player.fall()
             }
         }
 
