fc873f598d847a10fd6dc319bfe9f0feda0fa6a8
Harrison Kim <29724111+hhkimmm9@users.noreply.github.com>
2023-12-28 09:36:24 -0500

    Resolve flickering issue with background number image.

--- app/scripts/classes/BackgroundNumber.ts
+++ app/scripts/classes/BackgroundNumber.ts
@@ -3,28 +3,27 @@ class BackgroundNumber {
     private y: number
     private width: number
     private height: number
-    private zeroImage = new Image()
-    private oneImage = new Image()
+    private numberImage = new Image()
 
     constructor() {
         this.x = window.innerWidth
         this.y = Math.floor(Math.random() * 75)
-        this.width = 25
-        this.height = 25
+        this.width = 10
+        this.height = 15
 
-        this.zeroImage.src = '/images/zero.png'
-        this.oneImage.src = '/images/one.png'
-    }
-    draw(ctx: CanvasRenderingContext2D) {
         var zeroOrOne = Math.floor(Math.random() * 2)
 
+        if (zeroOrOne === 0)
+            this.numberImage.src = '/images/zero.png'
+        else
+            this.numberImage.src = '/images/one.png'
+    }
+    draw(ctx: CanvasRenderingContext2D) {
+
         if (ctx) {
-            ctx.fillStyle = 'yellow'
-            ctx?.fillRect(this.x, this.y, this.width, this.height)
-            if (zeroOrOne === 0)
-                ctx?.drawImage(this.zeroImage, this.x, this.y, this.width, this.height)
-            else
-                ctx?.drawImage(this.oneImage, this.x, this.y, this.width, this.height)
+            // ctx.fillStyle = 'yellow'
+            // ctx?.fillRect(this.x, this.y, this.width, this.height)
+            ctx?.drawImage(this.numberImage, this.x, this.y, this.width, this.height)
         }
     }
     moveForward() {
