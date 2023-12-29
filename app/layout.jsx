0c3eec40c652e31f5b7e445d7a94679b50c21851
Harrison Kim <29724111+hhkimmm9@users.noreply.github.com>
2023-12-29 13:21:54 -0500

    Enable metadata by converting the outermost layout into a server component.

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,64 +1,33 @@
-'use client'
-
-import React, { useState } from 'react'
-import { Rubik } from 'next/font/google'
-import TopNavbar from '@/layouts/TopNavbar'
-import Footer from '@/layouts/Footer'
 import './globals.css'
 import 'semantic-ui-css/semantic.min.css'
-import { FaAngleUp } from "react-icons/fa6";
+import { Rubik } from 'next/font/google'
+
+import React from 'react'
+
+import TopNavbar from '@/layouts/TopNavbar'
+import Main from '@/layouts/Main'
+import Footer from '@/layouts/Footer'
 
 const rubik = Rubik({ subsets: ['latin'] })
 
-// export const metadata = {
-//       title: 'Hey, Dev Kim!',
-//       description: 'My Personal Website',
-// }
+export const metadata = {
+    title: 'harrisonkim.dev',
+    description: 'Harrison Kim\'s Personal Website.',
+}
 
 export default function RootLayout({ children }) {
-  const [toggleSidebar, setToggleSidebar] = useState(false)
+    return (
+        <html lang="en">
+            <head>
+                
+            </head>
+            <body className={`${rubik.className} min-w-min`}>
+                <TopNavbar />
 
-  const scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'  // This will create a smooth scrolling effect
-    })
-  }
-
-  return (
-    <html lang="en">
-      <head>
-          
-      </head>
-      <body className={rubik.className}>
-        <div className='min-w-min'>
-        <TopNavbar
-          emitCloseSidebar={(value) => setToggleSidebar(value) }
-        />
-
-        <div className={`
-          relative overflow-hidden pt-[52.5px]
-          ${ toggleSidebar === true ? 'hidden' : '' }
-        `}>
-          <div className={`   
-            min-h-[calc(100vh-(60px+76px))] flex flex-grow
-            md:min-h-[calc(100vh-(60px+172px))]
-          `}>
-            { children }
-          </div>
-
-          <button type="button" onClick={scrollToTop}
-            className='
-            fixed bottom-0 right-0 mb-8 mr-20 p-2
-            rounded-xl border-2 border-stone-800 bg-stone-200 hover:bg-stone-100
-          '>
-            <FaAngleUp />
-          </button>
-
-          <Footer />
-        </div>
-        </div>
-      </body>
-    </html>
-  )
+                <Main children={children} />
+                
+                <Footer />
+            </body>
+        </html>
+    )
 }
