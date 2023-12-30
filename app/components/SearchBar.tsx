48dbed6bf9c830e7b8ef37fe420fdb352674bf07
Harrison Kim <29724111+hhkimmm9@users.noreply.github.com>
2023-12-30 11:37:46 -0500

    Add searchBar component.

---
+++ app/components/SearchBar.tsx
@@ -0,0 +1,36 @@
+"use client"
+
+import React, { useState } from 'react'
+import { FaMagnifyingGlass } from "react-icons/fa6";
+
+const SearchBar = ({ searchSubmit }: { searchSubmit: Function }) => {
+    const [searchQuery, setsearchQuery] = useState('')
+
+    const emitSearchQuery = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        searchSubmit(searchQuery)
+        setsearchQuery('')
+    }
+
+    return (
+        <div className='my-8 flex justify-center'>
+            <form onSubmit={(e) => emitSearchQuery(e)} className='flex flex-row'>
+                <label htmlFor="searchQuery"></label>
+                <input
+                    type="text" id="searchQuery" name=""
+                    value={searchQuery} placeholder='Search...'
+                    onChange={e => setsearchQuery(e.target.value)}
+                    className='
+                        w-64 p-2 rounded-tl-xl rounded-bl-xl border bg-white
+                '/>
+                <button type="submit" className='
+                    p-2 rounded-tr-xl rounded-br-xl bg-gradient-to-bl from-stone-200
+                '>
+                <FaMagnifyingGlass />
+                </button>
+            </form>
+        </div>
+    )
+}
+
+export default SearchBar
