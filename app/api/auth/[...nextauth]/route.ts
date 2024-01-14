24a8f691f970ee47bd8a72e2a255bcd5bbe457dd
Harrison Kim <29724111+hhkimmm9@users.noreply.github.com>
2024-01-14 18:06:35 -0500

    Import a type named "NextAuthOptions" from the next-auth package.

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,8 @@
 import NextAuth from "next-auth/next"
-import CredentialsProvider from 'next-auth/providers/credentials'
+import { NextAuthOptions } from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
 
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
   providers: [
     CredentialsProvider({
       name: "Credentials",
@@ -57,54 +58,6 @@ export const authOptions = {
   },
 }
 
-const handler = NextAuth({
-  providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        // email: {
-        //   label: "Email",
-        //   type: "email",
-        // },
-        password: {
-          label: "Password",
-          type: "password",
-        }
-      },
-      async authorize(credentials, req) {
-        const res = await fetch(`/api/auth/login`, {
-          method: "POST",
-          body: JSON.stringify(credentials),
-          headers: { "Content-Type": "application/json" },
-        })
-        const user = await res.json()
-
-        if (res.ok && user) {
-          return user
-        }
-        return null
-      }
-    })
-  ],
-  session: {
-    strategy: "jwt",
-    maxAge: 30 * 24 * 60 * 60, // 30 days
-  },
-  pages: {
-    signIn: '/admin/login'
-  },
-  callbacks: {
-    async jwt({ token, user, trigger, session }) {
-      if (trigger === "update") {
-        return { ...token, ...session.user, }
-      }
-      return { ...token, ...user, }
-    },
-    async session({ session, token }) {
-      session.user = token
-      return { ...session }
-    }
-  },
-})
+const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
