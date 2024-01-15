62dedc5a16f186c69e35e7d55e743670826b9b7d
Harrison Kim <29724111+hhkimmm9@users.noreply.github.com>
2024-01-15 11:30:08 -0500

    Integrate a sign-out button for user logout functionality.

--- app/admin/login/page.tsx
+++ app/admin/login/page.tsx
@@ -3,9 +3,12 @@
 import React, { useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { signIn } from 'next-auth/react'
+import { useSession, signOut } from 'next-auth/react'
 
 const AdminLogin = () => {
   const router = useRouter()
+
+  const { data: session, status } = useSession({ required: false })
 
   const [passwordInput, setPasswordInput] = useState('')
   const [confirmPasswordInput, setConfirmPasswordInput] = useState('')
@@ -39,34 +42,52 @@ const AdminLogin = () => {
       <div className='w-min md:w-1/3 px-12 py-8 rounded-lg border border-stone-200 shadow-md'>
         <h1 className='whitespace-nowrap font-bold text-2xl text-stone-700'>Admin</h1>
         
-        <form onSubmit={(e) => submitCredential(e)} className='my-8 flex flex-col'>
-          <div className='flex flex-col'>
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              id="password"
-              value={passwordInput}
-              onChange={(e) => setPasswordInput(e.target.value)}
-              className='my-2 p-2 border rounded-md'
-            />
-          </div>
-          <div className='flex flex-col'>
-            <label htmlFor="confirmPassword">Confirm Password</label>
-            <input
-              type="password"
-              id="confirmPassword"
-              value={confirmPasswordInput}
-              onChange={(e) => setConfirmPasswordInput(e.target.value)}
-              className='my-2 p-2 border rounded-md'
-            />
-          </div>
-          <div className='flex justify-center'>
-            <button type="submit" className='
-              w-full mt-8 py-2 rounded-md bg-stone-200 hover:bg-stone-300
+        { session ? (
+          <div className='my-8 flex flex-col justify-center'>
+            <button onClick={() => signOut()} className='
+              w-min
+              mx-auto
+              p-3
+              rounded-md
+              border
+              whitespace-nowrap
               font-semibold
-            '>Submit</button>
+            text-stone-800
+            bg-stone-100
+            hover:bg-stone-200
+            '>Sign out</button>
           </div>
-        </form>
+        ) : (
+          <form onSubmit={(e) => submitCredential(e)} className='my-8 flex flex-col'>
+            <div className='flex flex-col'>
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                value={passwordInput}
+                onChange={(e) => setPasswordInput(e.target.value)}
+                className='my-2 p-2 border rounded-md'
+              />
+            </div>
+            <div className='flex flex-col'>
+              <label htmlFor="confirmPassword">Confirm Password</label>
+              <input
+                type="password"
+                id="confirmPassword"
+                value={confirmPasswordInput}
+                onChange={(e) => setConfirmPasswordInput(e.target.value)}
+                className='my-2 p-2 border rounded-md'
+              />
+            </div>
+            <div className='flex justify-center'>
+              <button type="submit" className='
+                w-full mt-8 py-2 rounded-md bg-stone-200 hover:bg-stone-300
+                font-semibold
+              '>Submit</button>
+            </div>
+          </form>
+        )}
+        
       </div>
     </div>
   )
