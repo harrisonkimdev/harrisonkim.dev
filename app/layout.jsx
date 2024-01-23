2bfa2c75b36aa0e25c67f39861afe0341e8292d2
Harrison Kim <29724111+hhkimmm9@users.noreply.github.com>
2024-01-22 19:43:14 -0500

    Delete the "Provider" component.

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,11 +1,9 @@
-
 import React from 'react'
 
 import { Rubik } from 'next/font/google'
 import './globals.css'
 import 'semantic-ui-css/semantic.min.css'
 
-import Provider from '@/components/Provider'
 import TopNavbar from '@/layouts/TopNavbar'
 import MainComponent from '@/layouts/MainComponent'
 import Footer from '@/layouts/Footer'
@@ -21,19 +19,17 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${rubik.className}`}>
-        <Provider>
-          <div className='flex flex-col'>
-            <TopNavbar />
+        <div className='flex flex-col'>
+          <TopNavbar />
 
-            <div className='mt-8 flex-grow'>
-              <MainComponent>
-                { children }
-              </MainComponent>
-            </div>
-            
-            <Footer />
+          <div className='mt-8 flex-grow'>
+            <MainComponent>
+              { children }
+            </MainComponent>
           </div>
-        </Provider>
+          
+          <Footer />
+        </div>
       </body>
     </html>
   )
