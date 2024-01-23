dcc4ec74ba39e450556502ce7cec12f9419bf50b
Harrison Kim <29724111+hhkimmm9@users.noreply.github.com>
2024-01-22 20:24:55 -0500

    Move the "Sidebar" component into the main component.

--- app/layouts/MainComponent.tsx
+++ app/layouts/MainComponent.tsx
@@ -1,9 +1,13 @@
 'use client'
 
-import React, { ReactNode } from 'react'
+import React, { useState, ReactNode } from 'react'
 import { FaAngleUp } from "react-icons/fa6";
 
+import Sidebar from '@/layouts/Sidebar'
+
 const MainComponent = ({ children }: { children: ReactNode }) => {
+  const [showSidebar, setShowSidebar] = useState(false)
+
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
@@ -13,15 +17,26 @@ const MainComponent = ({ children }: { children: ReactNode }) => {
 
   return (
     <div className="relative">
-      { children }
+      { showSidebar ? (
+        // small screen devices only
+        <div className='absolute top-0 left-0 right-0'>
+          <Sidebar
+            closeSidebar={() => { setShowSidebar(false) }}
+          />
+        </div>
+      ) : (
+        <>
+          { children }
 
-      <button type="button" onClick={() => scrollToTop()}
-        className='
-          fixed bottom-0 right-0 mb-8 mr-20 p-2
-          rounded-xl border-2 border-stone-800 bg-stone-200 hover:bg-stone-100
-      '>
-        <FaAngleUp />
-      </button>
+          <button type="button" onClick={() => scrollToTop()}
+            className='
+              fixed bottom-0 right-0 mb-8 mr-20 p-2
+              rounded-xl border-2 border-stone-800 bg-stone-200 hover:bg-stone-100
+          '>
+            <FaAngleUp />
+          </button>
+        </>
+      )}
     </div>
   )
 }
