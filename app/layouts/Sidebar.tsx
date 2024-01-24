4ab40e07bbc4d4c96f88470c9beff5fb9475cdfe
Harrison Kim <29724111+hhkimmm9@users.noreply.github.com>
2024-01-24 16:51:57 -0500

    Improve styling in the sidebar to enhance the overall user experience.

--- app/layouts/Sidebar.tsx
+++ app/layouts/Sidebar.tsx
@@ -3,38 +3,38 @@ import Link from 'next/link'
 import { FaRegTimesCircle } from "react-icons/fa";
 
 // Only visible on devices with smaller screen
-const Sidebar = ({
+const SideBar = ({
   closeSidebar
 } : {
   closeSidebar: Function
 }) => {
   return (
     <div className='
-      min-h-screen py-2 flex flex-col gap-6 bg-stone-100
+      min-h-screen flex flex-col gap-6 bg-stone-100
       text-lg text-stone-900
     '>
       {/* close icon */}
-      <div onClick={() => closeSidebar()} className='flex justify-end mt-1 mr-3'>
+      <div onClick={() => closeSidebar()} className='m-5 flex justify-end'>
         <FaRegTimesCircle className='w-6 h-6 text-stone-600' />
       </div>
 
       {/* tabs */}
-      <div className='flex flex-col gap-8 px-6'>
+      <div className='flex flex-col gap-8 pl-12'>
         <Link href="/" onClick={() => closeSidebar()}>
           <p className='
-            font-medium text-stone-500 hover:text-stone-600
+            text-3xl font-medium text-stone-500 hover:text-stone-600
           '>Home</p>
         </Link>
       
         <Link href="/projects" onClick={() => closeSidebar()}>
           <p className='
-            font-medium text-stone-500 hover:text-stone-600
+            text-3xl font-medium text-stone-500 hover:text-stone-600
           '>Projects</p>
         </Link>
       
         <Link href="/blog" onClick={() => closeSidebar()}>
           <p className='
-            font-medium text-stone-500 hover:text-stone-600
+            text-3xl font-medium text-stone-500 hover:text-stone-600
           '>Blog</p>
         </Link>
       </div>
@@ -42,4 +42,4 @@ const Sidebar = ({
   )
 }
 
-export default Sidebar
+export default SideBar
