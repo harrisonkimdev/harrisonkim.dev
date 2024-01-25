71aded3557245cb5e37732cb4d42694b4df18a23
Harrison Kim <29724111+hhkimmm9@users.noreply.github.com>
2024-01-25 16:54:21 -0500

    Adjust the height of the Contact Me page to resolve layout issues.

--- app/(pages)/contact-me/page.tsx
+++ app/(pages)/contact-me/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ContactMePage = () => {
   return (
-    <div className='max-w-5xl mx-auto p-8 md:p-16'>
+    <div className='max-w-5xl h-screen mx-auto'>
       {/* Google Calendar Appointment Scheduling begin */}
       <iframe src="https://calendar.google.com/calendar/appointments/schedules/AcZssZ0sKX_f3sdmknsuWO5J1QnYAUPwLwOSJvZVjviJJzgsktfnjf30lMhvIJrLHoIwYJOGaf6LTKNA?gv=true"
         className="w-full h-full border-none"
