63b6d243b96e0fed06a3ec248c978d58fd33ef9e
Harrison Kim <29724111+hhkimmm9@users.noreply.github.com>
2024-01-31 16:59:44 -0500

    Incorporate `react-toastify` into the `AddComment` component along with input validation.

--- app/(pages)/blog/(components)/AddComment.tsx
+++ app/(pages)/blog/(components)/AddComment.tsx
@@ -1,6 +1,8 @@
 'use client'
 
 import React, { useState } from 'react'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
 
 const AddComment = (
   {
@@ -15,24 +17,46 @@ const AddComment = (
   const [comment, setComment] = useState("")
   const [password, setPassword] = useState("")
 
+  const notifyCommentAdded = () => toast.success("You just added a comment!", {
+    position: 'bottom-right'
+  })
+  const notifyCommentFailed = (err: any) => toast.error(err, {
+    position: 'bottom-right'
+  })
+  const notifyInvalidComment = () => toast.warn('Please fill all the input fields.', {
+    position: 'bottom-right'
+  })
+
   const addComment = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
 
     // TODO: validation checks
 
-    const res = await fetch('/api/comments', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        blogId, writer, comment, password
-      })
-    })
-    const data = await res.json()
+    if (writer.length == 0 || comment.length == 0 || password.length == 0) {
+      notifyInvalidComment()
+      return
+    }
 
-    resetInputFields()
-    fetchComments()
+    try {
+      const res = await fetch('/api/comments', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          blogId, writer, comment, password
+        })
+      })
+  
+      if (res.ok) {
+        resetInputFields()
+        fetchComments()
+  
+        notifyCommentAdded()
+      }
+    } catch (err) {
+      notifyCommentFailed(err)
+    }
   }
 
   const resetInputFields = () => {
@@ -42,48 +66,52 @@ const AddComment = (
   }
 
   return (
-    <div>
-      <form onSubmit={(e) => addComment(e)}
-        className='mb-8 p-4'
-      >
-        <div className='flex flex-col md:flex-row md:justify-between'>
-          <div className='flex flex-col'>
-            <label htmlFor="writer">Writer</label>
-            <input type="text" id="writer"
-              value={writer} onChange={(e) => setWriter(e.target.value)}
-              className='p-2 rounded-md'
+    <>
+      <ToastContainer />
+    
+      <div>
+        <form onSubmit={(e) => addComment(e)}
+          className='mb-8 p-4'
+        >
+          <div className='flex flex-col md:flex-row md:justify-between'>
+            <div className='flex flex-col'>
+              <label htmlFor="writer">Writer</label>
+              <input type="text" id="writer"
+                value={writer} onChange={(e) => setWriter(e.target.value)}
+                className='p-2 rounded-md'
+              />
+            </div>
+            <div className='flex flex-col'>
+              <label htmlFor="">Password</label>
+              <input type="password" id="password"
+                value={password} onChange={(e) => setPassword(e.target.value)}
+                className='p-2 rounded-md'
+              />
+            </div>
+          </div>
+          
+          <div className='mt-2'>
+            <label htmlFor="content">Comment</label>
+            <textarea id="content"
+              value={comment} onChange={(e) => setComment(e.target.value)}
+              className='w-full p-2 rounded-md'
             />
           </div>
-          <div className='flex flex-col'>
-            <label htmlFor="">Password</label>
-            <input type="password" id="password"
-              value={password} onChange={(e) => setPassword(e.target.value)}
-              className='p-2 rounded-md'
-            />
-          </div>
-        </div>
-        
-        <div className='mt-2'>
-          <label htmlFor="content">Comment</label>
-          <textarea id="content"
-            value={comment} onChange={(e) => setComment(e.target.value)}
-            className='w-full p-2 rounded-md'
-          />
-        </div>
 
-        <div className='mt-6 flex justify-center'>
-          <button type="submit" className='
-            w-full
-            py-2
-            rounded-md
-            border
-          border-stone-300
-          bg-stone-200
-          hover:bg-stone-300
-          '>Add</button>
-        </div>
-      </form>
-    </div>
+          <div className='mt-6 flex justify-center'>
+            <button type="submit" className='
+              w-full
+              py-2
+              rounded-md
+              border
+            border-stone-300
+            bg-stone-200
+            hover:bg-stone-300
+            '>Add</button>
+          </div>
+        </form>
+      </div>
+    </>
   )
 }
 
