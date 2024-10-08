bfc59f72ad99417d59f5b55cea19055a24c2f7fc
Harrison Kim <29724111+hhkimmm9@users.noreply.github.com>
2024-10-08 09:57:02 -0400

    Hide the game from the footer.

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -1,24 +1,15 @@
-import { usePathname } from 'next/navigation'
 import Link from 'next/link'
 import Image from 'next/image'
-import DinosaurGame from '@/components/DinosaurGame'
+// import DinosaurGame from '@/components/DinosaurGame'
 
 const Footer = () => {
-  const pathname = usePathname()
-
   return (
-    <div className='
+    <footer className='
       p-8 flex flex-col gap-16 justify-center bg-stone-200
     '>
       <div className='
         flex flex-row gap-8 justify-center items-center
       '>
-        <Link href='/'>
-          <Image src='/images/logos/dev-kim-transparent.png' alt='Dev Kim Logo'
-            width={128} height={128} className='object-contain w-28 lg:w-32'
-          />
-        </Link>
-
         {/* links */}
         <Link href='https://www.linkedin.com/in/harrison-kim-b246a5175/'
           className='flex flex-row gap-2 items-center'
@@ -42,15 +33,15 @@ const Footer = () => {
         </Link>
       </div>
 
-      {/* dinosaur game */}
-      { pathname === '/' ? (
+      {/* TODO: move the game to a dedicated page */}
+      {/* { pathname === '/' ? (
         <div className='hidden md:block'>
           <DinosaurGame />
         </div>
       ) : (
         <></>
-      )}
-    </div>
+      )} */}
+    </footer>
   )
 }
 
