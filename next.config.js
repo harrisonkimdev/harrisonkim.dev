b81d02d96ede26a4f854fceb6f9b8fb6d78f40b5
Harrison Kim <29724111+hhkimmm9@users.noreply.github.com>
2024-10-08 21:40:12 -0400

    Disable font optimization in next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   experimental: {
     // appDir: true,
     // serverComponentsExternalPackages: ["mongoose"],
-    optimizeFonts: false,
+    // optimizeFonts: false,
   },
   webpack: (config) => {
     config.experiments = { ...config.experiments, topLevelAwait: true }
