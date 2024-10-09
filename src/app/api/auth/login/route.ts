0e6e7b67bacb6fbf539758f72d359dfe90ca48d7
Harrison Kim <29724111+hhkimmm9@users.noreply.github.com>
2024-10-08 21:20:03 -0400

    Refactor bcrypt import and fix server configuration error.

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server'
 import { connectToDB } from '@/utils/db'
-import * as bcrypt from 'bcryptjs'
+import bcrypt from 'bcryptjs' // Ensure correct import
 import { signJwtAccessToken } from '@/utils/jwt'
 
 export const POST = async (req: Request) => {
@@ -15,28 +15,29 @@ export const POST = async (req: Request) => {
       return NextResponse.json({ message: 'Missing password input.' }, { status: 400 })
     }
 
-    const salt = await bcrypt.genSalt()
-    
-    if (process.env.ADMIN_PASSWORD) {
-      const systemPassword = await bcrypt.hash(process.env.ADMIN_PASSWORD, salt)
-
-      const isMatch = await bcrypt.compare(password, systemPassword)
-      if (!isMatch) return NextResponse.json({ error: 'Password doesn\'t match.' }, { status: 401 })
-
-      const userData = {
-        name: 'Harrison Kim',
-        email: 'harrisonkimdev@gmail.com',
-      }
-
-      // No user data available at the moment.
-      const accessToken = signJwtAccessToken(userData)
-
-      return NextResponse.json({
-        accessToken,
-        ...userData
-      }, { status: 200 })
+    if (!process.env.ADMIN_PASSWORD) {
+      return NextResponse.json({ message: 'Server configuration error.' }, { status: 500 })
     }
+
+    const salt = await bcrypt.genSalt()
+    const systemPassword = await bcrypt.hash(process.env.ADMIN_PASSWORD, salt)
+
+    const isMatch = await bcrypt.compare(password, systemPassword)
+    if (!isMatch) return NextResponse.json({ error: 'Password doesn\'t match.' }, { status: 401 })
+
+    const userData = {
+      name: 'Harrison Kim',
+      email: 'harrisonkimdev@gmail.com',
+    }
+
+    // No user data available at the moment.
+    const accessToken = signJwtAccessToken(userData)
+
+    return NextResponse.json({
+      accessToken,
+      ...userData
+    }, { status: 200 })
   } catch (err) {
-    return NextResponse.json({ error: err }, { status: 500 })
+    return NextResponse.json({ error: (err as Error).message }, { status: 500 })
   }
 }
