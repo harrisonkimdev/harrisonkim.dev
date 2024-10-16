b31176eb379335d5fdbfae5fe1cacfea764e2c1e
Harrison Kim <29724111+hhkimmm9@users.noreply.github.com>
2024-10-16 16:20:58 -0400

    Refactor Blogs page.

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,90 +1,63 @@
-'use client'
+"use client"
 
-import { IBlog } from '@/interfaces'
-import { useState, useEffect } from 'react'
-
-import PaginationNavigator from '@/components/PaginationNavigator'
-import SearchBar from '@/components/SearchBar'
-import Blog from './(components)/Blog'
+import { IBlog } from "@/interfaces"
+import { useState, useEffect } from "react"
+import Blog from "./(components)/Blog"
 
 const BlogIndexPage = () => {
   const [currentPage, setCurrentPage] = useState<number>(1)
   const [lastPage, setLastPage] = useState<number>(1)
-  const [pageSize, setPageSize] = useState<string>('4')
-  const [blogData, setBlogData] = useState<IBlog[] | undefined>(undefined)
+  const [pageSize, setPageSize] = useState<string>("4")
+  const [blogs, setBlogs] = useState<IBlog[] | undefined>(undefined)
 
-  const fetchBlog = async () => {
+  const fetchBlogs = async (searchQuery: string = "") => {
     try {
-      var currentPageString = currentPage.toString()
       const params = new URLSearchParams({
-        currentPage: currentPageString,
-        pageSize
+        currentPage: currentPage.toString(),
+        pageSize,
+        ...(searchQuery && { searchQuery })
       })
-      const res = await fetch(`/api/blog?${params}`)
-      const data = await res.json()
-      setBlogData(data.blog)
-      setLastPage(data.lastPage)
+      const res = await fetch(`/api/blogs?${params}`)
+      const { blogs, lastPage } = await res.json()
+      setBlogs(blogs)
+      setLastPage(lastPage)
     } catch (err: any) {
       console.error(err.message)
     }
   }
 
   useEffect(() => {
-    fetchBlog()
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [currentPage])
+    fetchBlogs()
+  }, [currentPage, pageSize])
 
-  const handleSearch = async (searchQuery: string) => {
-    if (searchQuery.length === 0) {
-      fetchBlog()
-      return
-    }
-
-    try {
-      var currentPageString = currentPage.toString()
-      const params = new URLSearchParams({
-        currentPageString,
-        pageSize,
-        searchQuery
-      })
-      const res = await fetch(`/api/blog?${params}`,
-        {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        }
-      )
-      const data = await res.json()
-      setBlogData(data.blog)
-    } catch (err: any) {
-      console.error(err.message)
-    }
+  const handleSearch = (searchQuery: string) => {
+    fetchBlogs(searchQuery)
   }
 
   return (
     <>
-      <SearchBar searchSubmit={(searchQuery: string) => handleSearch(searchQuery)} />
-
-      <div className='mt-12'>
-        { blogData && blogData.length > 0 ? (
-          <div className='
-            my-8 grid grid-col-1 gap-8 sm:grid-cols-2 sm:gap-4
-          '>
-            { blogData?.map((blog: IBlog) => <Blog blogData={blog} key={blog._id} />) }
+      {/* { blogs && blogs?.length > 0 ? ( */}
+      { 1 ? (
+        <>
+          <h2 className="
+            font-mono font-light text-center text-lime-400
+          "> Blogs </h2>
+          <div className="
+            my-8 grid
+            grid-col-1 sm:grid-cols-2
+            gap-8 sm:gap-4
+          ">
+            { blogs?.map((blog: IBlog) => <Blog blogData={blog} key={blog._id} />) }
           </div>
-        ) : (
-          <div className='my-8 text-2xl text-center'>No Blog Posts Just Yet...</div>
-        )}
-      </div>
-
-      { blogData && blogData.length > 0 && (
-        <div className='mt-16 flex flex-col justify-end'>
-          <PaginationNavigator
-            currentPage={currentPage}
-            lastPage={lastPage}
-            setCurrentPage={(pageNum: number) => setCurrentPage(pageNum)}
-          />
+        </>
+      ) : (
+        <div className="
+          h-full
+          flex flex-col items-center justify-center
+          font-mono whitespace-nowrap
+          text-xl text-center text-lime-400
+        ">
+          No Blog Posts Yet...
         </div>
       )}
     </>
