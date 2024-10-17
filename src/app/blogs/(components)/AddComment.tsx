0c9ffbcc5eb9e4157cbef3bfec2eff61177748d5
Harrison Kim <29724111+hhkimmm9@users.noreply.github.com>
2024-10-17 16:39:09 -0400

    Refactor AddComment component to improve code structure and readability

--- src/app/blogs/(components)/AddComment.tsx
+++ src/app/blogs/(components)/AddComment.tsx
@@ -1,15 +1,3 @@
-'use client'
-
-import 'react-toastify/dist/ReactToastify.css'
-import { ToastContainer, toast } from 'react-toastify'
-import { useState } from 'react'
-
-type TAddCommentProps = {
-  blogId: string | undefined,
-  fetchComments: () => void,
-  hideAddComment: () => void
-}
-
 /**
  * AddComment component allows users to add comments to a blog post.
  * 
@@ -28,23 +16,44 @@ type TAddCommentProps = {
  * It also includes form validation to ensure all input fields are filled before submission.
  */
 
-const AddComment = ({ blogId, fetchComments, hideAddComment } : TAddCommentProps) => {
-  const [writer, setWriter] = useState('')
-  const [comment, setComment] = useState('')
-  const [password, setPassword] = useState('')
+"use client"
 
-  const notifyCommentAdded = () => toast.success('You just added a comment!', {
-    position: 'bottom-right'
+import "react-toastify/dist/ReactToastify.css"
+import { ToastContainer, toast } from "react-toastify"
+import { useState } from "react"
+
+interface IAddCommentProps {
+  blogId?: string;
+  fetchComments: () => void;
+  hideAddComment: () => void;
+}
+
+const AddComment = ({ blogId, fetchComments, hideAddComment }: IAddCommentProps) => {
+  const [formState, setFormState] = useState({
+    writer: "",
+    comment: "",
+    password: ""
+  })
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setFormState(prevState => ({ ...prevState, [name]: value }));
+  }
+
+  const notifyCommentAdded = () => toast.success("You just added a comment!", {
+    position: "bottom-right"
   })
   const notifyCommentFailed = (err: any) => toast.error(err, {
-    position: 'bottom-right'
+    position: "bottom-right"
   })
-  const notifyInvalidComment = () => toast.warn('Please fill all the input fields.', {
-    position: 'bottom-right'
+  const notifyInvalidComment = () => toast.warn("Please fill all the input fields.", {
+    position: "bottom-right"
   })
-
+  
   const addComment = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
+
+    const { writer, comment, password } = formState;
 
     if (!writer || !comment || !password) {
       notifyInvalidComment()
@@ -52,27 +61,24 @@ const AddComment = ({ blogId, fetchComments, hideAddComment } : TAddCommentProps
     }
 
     try {
-      const res = await fetch('/api/comments', {
-        method: 'POST',
+      const res = await fetch("/api/comments", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({ blogId, writer, comment, password })
       })
   
       if (res.ok) {
-        setWriter('')
-        setComment('')
-        setPassword('')
-        
+        setFormState({ writer: '', comment: '', password: '' })
         fetchComments()
-        notifyCommentAdded()
+        // notifyCommentAdded()
       } else {
         const errorText = await res.text()
-        notifyCommentFailed(errorText)
+        // notifyCommentFailed(errorText)
       }
     } catch (err: any) {
-      notifyCommentFailed(err.message || 'An error occurred')
+      // notifyCommentFailed(err.message || "An error occurred")
     }
   }
 
@@ -81,51 +87,51 @@ const AddComment = ({ blogId, fetchComments, hideAddComment } : TAddCommentProps
       <ToastContainer />
     
       <form onSubmit={(e) => addComment(e)}>
-        <div className='grid grid-cols-5 gap-6'>
-          <div className='col-span-3 flex flex-col'>
-            <label htmlFor='writer' className='text-lime-400'>Writer</label>
-            <input type='text' id='writer'
-              value={writer} onChange={(e) => setWriter(e.target.value)}
-              className='
+        <div className="grid grid-cols-5 gap-6">
+          <div className="col-span-3 flex flex-col">
+            <label htmlFor="writer" className="text-lime-400">Writer</label>
+            <input type="text" name="writer"
+              value={formState.writer} onChange={handleChange}
+              className="
                 p-2 bg-black text-lime-400
                 border-b border-dashed border-lime-400 outline-none
-            '/>
+            "/>
           </div>
-          <div className='col-span-2 flex flex-col'>
-            <label htmlFor='password' className='text-lime-400'>Password</label>
-            <input type='password' id='password'
-              value={password} onChange={(e) => setPassword(e.target.value)}
-              className='
+          <div className="col-span-2 flex flex-col">
+            <label htmlFor="password" className="text-lime-400">Password</label>
+            <input type="password" name="password"
+              value={formState.password} onChange={handleChange}
+              className="
                 p-2 bg-black text-lime-400
                 border-b border-dashed border-lime-400 outline-none
-            '/>
+            "/>
           </div>
         </div>
         
-        <div className='mt-5 flex flex-col gap-2'>
-          <label htmlFor='content' className='text-lime-400'>Comment</label>
-          <textarea id='content' value={comment}
-            onChange={(e) => setComment(e.target.value)}
-            className='
+        <div className="mt-5 flex flex-col gap-2">
+          <label htmlFor="content" className="text-lime-400">Comment</label>
+          <textarea name="content" value={formState.comment}
+            onChange={handleChange}
+            className="
               w-full h-28 p-2 rounded-md bg-black
               border border-dashed border-lime-400
               outline-none resize-none overflow-auto
               text-lime-400
-            '
+            "
           />
         </div>
 
-        <div className='mt-6 grid grid-cols-2 gap-3'>
-          <button onClick={() => hideAddComment()} className='
+        <div className="mt-6 grid grid-cols-2 gap-3">
+          <button onClick={() => hideAddComment()} className="
             py-3 rounded-md
             border border-dashed border-lime-400
             text-lime-400
-          '> Actually, nvm </button>
-          <button type='submit' className='
+          "> Actually, nvm </button>
+          <button type="submit" className="
             py-3 rounded-md
             border border-dashed border-lime-400
             text-lime-400
-          '> Add </button>
+          "> Add </button>
         </div>
       </form>
     </>
