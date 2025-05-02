54a79674c738de520c96179066a5963a154818c0
Harrison Kim <29724111+hhkimmm9@users.noreply.github.com>
2025-05-02 15:48:58 -0400

    fix: Resolve dependency issues - Remove semantic-ui-react from Loader, install react-quill for blog editor

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import { Dimmer, Loader, Image, Segment } from 'semantic-ui-react'
 
 const LoaderComponent = () => {
   return (
     <div className='min-h-screen flex flex-col justify-center'>
-      <Loader active size='medium' inline='centered'>Loading</Loader>
+      <div className="w-10 h-10 border-t-2 border-primary rounded-full animate-spin"></div>
     </div>
   )
 }
